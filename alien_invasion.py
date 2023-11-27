819e0ea5d1bd48b5b8c15df02f49dc88835c4cf8
Andriy <andriyshrub@gmail.com>
2023-11-27 19:31:27 +0200

    Create Ship and moving.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,26 +2,54 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+#from BubuDudu import BubuDudu
+
 
 class AlienInvasion:
     """Класс для управления  ресурсами и поведения игры."""
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        #self.bubu_dudu = BubuDudu(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # отображение последнего прорисованого экрана.
-            pygame.display.flip()
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и собития мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        self.ship.update()
+        #self.bubu_dudu.bltime()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
